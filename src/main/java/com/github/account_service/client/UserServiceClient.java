cf611c9bb2dfecca8ef4da5f209fed3c02e76c46
Eljan Najafov <eljan.najafov02@mail.ru>
2024-09-15 18:11:49 +0400

    adding client

--- src/main/java/com/github/account_service/client/UserServiceClient.java
+++ src/main/java/com/github/account_service/client/UserServiceClient.java
@@ -1,6 +1,6 @@
 package com.github.account_service.client;
 
-import com.github.account_service.user.UserDto;
+import com.github.account_service.dto.user.UserDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
