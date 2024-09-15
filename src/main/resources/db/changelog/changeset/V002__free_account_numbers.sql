2406f33e8c70a7b02cde9f5061ad8a087a4b6ad9
Eljan Najafov <eljan.najafov02@mail.ru>
2024-09-15 17:59:10 +0400

    migrations

--- src/main/resources/db/changelog/changeset/V002__free_account_numbers.sql
+++ src/main/resources/db/changelog/changeset/V002__free_account_numbers.sql
@@ -1,17 +1,17 @@
-CREATE TABLE free_account_numbers
+CREATE TABLE IF NOT EXISTS free_account_numbers
 (
-    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    account_type smallint NOT NULL,
+    id             bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    account_type   smallint                                         NOT NULL,
     account_number VARCHAR(20) CHECK (LENGTH(account_number) >= 12) NOT NULL,
     CONSTRAINT uc_account_type_account_number UNIQUE (account_type, account_number)
 );
 CREATE UNIQUE INDEX account_type_number_idx ON free_account_numbers (account_type, account_number);
 
-CREATE TABLE account_numbers_sequence
+CREATE TABLE IF NOT EXISTS account_numbers_sequence
 (
-    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    account_type smallint NOT NULL,
-    current_count BIGINT NOT NULL DEFAULT 0
+    id            bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    account_type  smallint NOT NULL,
+    current_count BIGINT   NOT NULL DEFAULT 0
 );
 CREATE UNIQUE INDEX account_numbers_sequence_ind ON account_numbers_sequence (account_type, current_count);
 
--- src/main/resources/db/changelog/changeset/V003__request.sql
+++ src/main/resources/db/changelog/changeset/V003__request.sql
@@ -1,15 +1,15 @@
-CREATE TABLE request (
-                         id bigint NOT NULL,
-                         idempotent_token UUID,
-                         user_id bigint NOT NULL,
-                         username varchar(64) UNIQUE NOT NULL,
-                         request_type smallint NOT NULL,
-                         lock_value bigint NOT NULL,
-                         active boolean NOT NULL,
-                         input_data JSONB,
-                         request_status varchar(128) NOT NULL,
-                         details varchar(128),
-                         created_at timestamptz DEFAULT current_timestamp,
-                         updated_at timestamptz DEFAULT current_timestamp,
-                         version bigint NOT NULL
+CREATE TABLE request
+(
+    id               bigint       NOT NULL,
+    idempotent_token UUID,
+    user_id          varchar(64)  NOT NULL,
+    request_type     smallint     NOT NULL,
+    lock_value       varchar(128) NOT NULL,
+    active           boolean      NOT NULL,
+    input_data       text,
+    status           varchar(128) NOT NULL,
+    status_details   text,
+    created_at       timestamptz,
+    updated_at       timestamptz,
+    version          bigint       NOT NULL
 );
